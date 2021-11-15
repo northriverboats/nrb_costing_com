74f05aca726708976289adacc9bea1d17d8b44df
Fredrick W. Warren <fred.warren@gmail.com>
2021-11-14 22:15:12 -0800

    read models spreadsheet

---
+++ models.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# vim expandtab shiftwidth=2 softtabstop=2
+"""
+Load model data from sheet
+
+Pass in master_file return data structure
+"""
+from dataclasses import dataclass
+from pathlib import Path
+import openpyxl  # pylint: disable=import-error
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.workbook.workbook import Workbook
+from utility import status_msg
+
+@dataclass
+class Model:
+    """Information on which sheets make a boat costing sheet"""
+    sheet1: str
+    sheet2: str
+    folder: str
+
+def load_models(models_file: Path) -> list[Model]:
+    """Build master list of sheets to combine to create costing sheets"""
+    status_msg('Loading Boat Models', 1)
+    xlsx: Workbook = openpyxl.load_workbook(
+        models_file.as_posix(), data_only=True)
+    sheet: Worksheet = xlsx.active
+    models: list[Model] = []
+    for row in sheet.iter_rows(min_row=2, max_col=3):
+        if not isinstance(row[0].value, str):
+            continue
+        model: Model = Model(
+            row[0].value,
+            row[1].value,
+            row[2].value)
+        models.append(model)
+        status_msg(f"  {model}", 2)
+    xlsx.close()
+    return models
+
+if __name__ == "__main__":
+    pass
