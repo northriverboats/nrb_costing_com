a6729168c366a58a4401b801ff6a4b984a55164c
Fredrick W. Warren <fred.warren@gmail.com>
2021-11-15 13:37:11 -0800

    reworking boms
    - converted from list to dict
    - extended bomparts to inlcude resource information
    - item detail verbosity moved to -vvv

--- boms.py
+++ boms.py
@@ -6,21 +6,31 @@ Load model data from sheet
 Pass in master_file return data structure
 """
 from dataclasses import dataclass, field
+from datetime import datetime
 from pathlib import Path
 from typing import Optional, Union
 from openpyxl import load_workbook # pylint: disable=import-error
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
+from resources import Resource
 from utility import status_msg
 
 @dataclass(order=True)
 class BomPart:
     """Part Information from Section of a BOM Parts Sheet"""
+    # pylint: disable=too-many-instance-attributes
     part: str
     qty: float = field(compare=False)
     smallest: Optional[float] = field(compare=False)
     biggest: Optional[float] = field(compare=False)
     percent: Optional[float] = field(compare=False)  # FT field
+    description: str = field(compare=False)
+    uom: str = field(compare=False)
+    unitprice: float = field(compare=False)
+    oem: str = field(compare=False)
+    vendorpart: str = field(compare=False)
+    vendor: str = field(compare=False)
+    updated: datetime = field(compare=False)
 
 @dataclass(order=True)
 class BomSection:
@@ -43,7 +53,7 @@ def find_excel_files_in_dir(base: Path) -> list[Path]:
     """get list of spreadsheets in folder"""
     return list(base.glob('[!~]*.xlsx'))
 
-def make_bom_part(row) -> BomPart:
+def make_bom_part(row, resources: dict[str, Resource]) -> BomPart:
     """Create bom part from row in spreadsheet"""
     qty: float = float(row[0].value)
     smallest: Optional[float] = (
@@ -53,7 +63,26 @@ def make_bom_part(row) -> BomPart:
     percent: Optional[float] = (
         None if row[3].value is None else float(row[3].value))
     part: str = str(row[5].value)
-    return BomPart(part, qty, smallest, biggest, percent)
+
+    # fww resolve at later point, we need to throw errors on fail
+    try:
+        resource = resources[part]
+    except KeyError:
+        resource = Resource(part, "Unknown", "EA", 0.0, "Unknown", part,
+                            "Unknown", datetime.now())
+
+    return BomPart(part,
+                   qty,
+                   smallest,
+                   biggest,
+                   percent,
+                   resource.description,
+                   resource.uom,
+                   resource.unitprice,
+                   resource.oem,
+                   resource.vendorpart,
+                   resource.vendor,
+                   resource.updated)
 
 def section_add_part(parts: dict[str, BomPart], part: BomPart) -> None:
     """Insert new part or add qty to existing part"""
@@ -74,7 +103,8 @@ def get_hull_sizes(sheet: Worksheet) -> list:
                 sizes.append(float(value))
     return sizes
 
-def get_bom_sections(sheet: Worksheet) -> list[BomSection]:
+def get_bom_sections(sheet: Worksheet,
+                     resources: dict[str, Resource]) -> list[BomSection]:
     """read in BOM items into sections"""
     sections: list[BomSection] = []
     for row in sheet.iter_rows(min_row=18,max_col=8):
@@ -86,17 +116,13 @@ def get_bom_sections(sheet: Worksheet) -> list[BomSection]:
                 sections.append(section)
             section: BomSection = BomSection(qty, {})
         elif isinstance(qty, (float, int)):
-            bom_part: BomPart = BomPart(
-                str(row[5].value),
-                float(row[0].value),
-                float(0 if row[1].value is None else row[1].value),
-                float(0 if row[2].value is None else row[2].value),
-                float(0 if row[3].value is None else row[3].value))
-            section.parts[str(row[5].value)] = bom_part
+            bom_part: BomPart = make_bom_part(row,  resources)
+            status_msg(f"    {bom_part}",3)
+            section_add_part(section.parts, bom_part)
     sections.append(section)
     return sections
 
-def load_bom(xlsx_file: Path) -> Bom:
+def load_bom(xlsx_file: Path, resources: dict[str, Resource]) -> Bom:
     """load individual BOM sheet"""
     status_msg(f'  {xlsx_file.name}', 2)
     xlsx: Workbook = load_workbook(xlsx_file.as_posix(), data_only=True)
@@ -109,19 +135,20 @@ def load_bom(xlsx_file: Path) -> Bom:
     biggest:float  = float(
         0 if sheet["M1"].value == "ANY" else sheet["G14"].value)
     sizes = [] if smallest == 0 else get_hull_sizes(sheet)
-    resources: list[BomSection] = get_bom_sections(sheet)
-    bom: Bom = Bom(name, beam, smallest, biggest, sizes, resources)
+    sections: list[BomSection] = get_bom_sections(sheet, resources)
+    bom: Bom = Bom(name, beam, smallest, biggest, sizes, sections)
     xlsx.close()
     return bom
 
-def load_boms(bom_folder: Path) -> list[Bom]:
+def load_boms(bom_folder: Path,
+              resources: dict[str, Resource]) -> dict[str, Bom]:
     """load all BOM sheets"""
     status_msg('Loading BOMs', 1)
     bom_files: list[Path] = find_excel_files_in_dir(bom_folder)
-    boms: list[Bom] = []
+    boms: dict[str, Bom] = {}
     for bom_file in bom_files:
-        bom = load_bom(bom_file)
-        boms.append(bom)
+        bom = load_bom(bom_file, resources)
+        boms[bom.name] = bom
     return boms
 
 if __name__ == "__main__":
