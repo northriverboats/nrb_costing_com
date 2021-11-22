9544e79d7506a0118153c4048536ca9a29019b22
Fredrick W Warren <fredwarren@gmail.com>
2021-11-22 15:33:21 -0800

    added database stub

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,6 +6,7 @@ Module Load
 from .boms import load_boms, Bom, BomPart, BomSection
 from .consumables import load_consumables, Consumable
 from .costingsheets import generate_sheets_for_all_models
+from .databases import load_from_database, save_to_database
 from .hourlyrates import load_hourly_rates, HourlyRate
 from .markups import load_mark_ups, MarkUp
 from .models import load_models, Model
---
+++ modules/databases.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# vim expandtab shiftwidth=2 softtabstop=2
+"""
+save/restore data from sqlite3
+
+Pass in master_file return data structure
+"""
+from pathlib import Path
+from .utilities import status_msg
+
+def load_from_database(db_file: Path)-> None:
+    """read data from database into objects"""
+    status_msg(db_file, 99)
+    status_msg("Loading Data....", 0)
+
+def save_to_database(db_file: Path)-> None:
+    """save data to database from objects"""
+    status_msg(db_file, 99)
+    status_msg("Saving Data....", 0)
+
+if __name__ == "__main__":
+    pass
