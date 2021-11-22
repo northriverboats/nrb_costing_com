076f4d5cde8f8e6314945495fa1199aa777867fc
Fredrick W Warren <fredwarren@gmail.com>
2021-11-22 14:55:59 -0800

    add Resources dict datatype

--- modules/resources.py
+++ modules/resources.py
@@ -6,9 +6,9 @@ Load model data from sheet
 Pass in master_file return data structure
 """
 from datetime import datetime
-from dataclasses_json import dataclass_json
 from dataclasses import dataclass, field
 from pathlib import Path
+from dataclasses_json import dataclass_json
 from openpyxl import load_workbook # pylint: disable=import-error
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
@@ -30,16 +30,22 @@ class Resource:
     vendor: str = field(compare=False)
     updated: datetime = field(compare=False)
 
+@dataclass_json
+@dataclass
+class Resources:
+    """Dictionary of all resources"""
+    resources: dict[str, Resource]
+
 def find_excel_files_in_dir(base: Path) -> list[Path]:
     """get list of spreadsheets in folder"""
     return list(base.glob('[!~]*.xlsx'))
 
-def load_resource_file(xlsx_file: Path) -> dict[str, Resource]:
+def load_resource_file(xlsx_file: Path) -> Resources:
     """Read resource sheet"""
     status_msg(f'  {xlsx_file.name}', 2)
     xlsx: Workbook = load_workbook(xlsx_file.as_posix(), data_only=True)
     sheet: Worksheet = xlsx.active
-    resources: dict[str, Resource] = {}
+    all_resources: Resources = Resources({})
     for row in sheet.iter_rows(min_row=2, max_col=8):
         if not isinstance(row[0].value, str):
             continue
@@ -52,12 +58,12 @@ def load_resource_file(xlsx_file: Path) -> dict[str, Resource]:
             row[5].value,
             row[6].value,
             row[7].value)
-        resources[row[0].value] = resource
+        all_resources.resources[row[0].value] = resource
         status_msg(f"    {resource}",3)
     xlsx.close()
-    return resources
+    return all_resources
 
-def load_resources(resource_folder: Path) -> dict[str, Resource]:
+def load_resources(resource_folder: Path) -> Resources:
     """Load all resource files"""
     status_msg('Loading Resources', 1)
     resource_files: list[Path] = [
@@ -65,10 +71,11 @@ def load_resources(resource_folder: Path) -> dict[str, Resource]:
         for sheet in find_excel_files_in_dir(resource_folder)
         if sheet.name.startswith('BOM ')]
 
-    resources: dict[str, Resource] = {}
+    all_resources: Resources = Resources({})
     for resource_file in resource_files:
-        resources.update(load_resource_file(resource_file))
-    return resources
+        all_resources.resources.update(
+            load_resource_file(resource_file).resources)
+    return all_resources
 
 if __name__ == "__main__":
     pass
