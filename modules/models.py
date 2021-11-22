180e964961ff7abe09d88fc7c1d0ab79aa130b8a
Fredrick W Warren <fredwarren@gmail.com>
2021-11-22 14:55:41 -0800

    add Models dict datatype

--- modules/models.py
+++ modules/models.py
@@ -7,11 +7,13 @@ Pass in master_file return data structure
 """
 from dataclasses import dataclass
 from pathlib import Path
+from dataclasses_json import dataclass_json
 from openpyxl  import load_workbook # pylint: disable=import-error
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
 from .utilities import status_msg
 
+@dataclass_json
 @dataclass
 class Model:
     """Information on which sheets make a boat costing sheet"""
@@ -19,13 +21,19 @@ class Model:
     sheet2: str
     folder: str
 
-def load_models(xlsx_file: Path) -> dict[str, Model]:
+@dataclass_json
+@dataclass
+class Models:
+    """Dictionary of all models"""
+    models: dict[str, Model]
+
+def load_models(xlsx_file: Path) -> Models:
     """Build master list of sheets to combine to create costing sheets"""
     status_msg('Loading Boat Models', 1)
     xlsx: Workbook = load_workbook(
         xlsx_file.as_posix(), data_only=True)
     sheet: Worksheet = xlsx.active
-    models: dict[str, Model] = {}
+    all_models: Models = Models({})
     for row in sheet.iter_rows(min_row=2, max_col=3):
         if not isinstance(row[0].value, str):
             continue
@@ -33,10 +41,10 @@ def load_models(xlsx_file: Path) -> dict[str, Model]:
             row[0].value,
             row[1].value,
             row[2].value)
-        models[model.sheet1] = model
+        all_models.models[model.sheet1] = model
         status_msg(f"    {model}", 3)
     xlsx.close()
-    return models
+    return all_models
 
 if __name__ == "__main__":
     pass
