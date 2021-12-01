704277baf7c08884e6d0d06982e244fc2116decc
Fredrick W Warren <fredwarren@gmail.com>
2021-12-01 09:12:59 -0800

    added dict dataclass

--- modules/hourlyrates.py
+++ modules/hourlyrates.py
@@ -7,36 +7,39 @@ Pass in master_file return data structure
 """
 from dataclasses import dataclass
 from pathlib import Path
-from dataclasses_json import dataclass_json
+from dataclasses_json import DataClassJsonMixin
 from openpyxl import load_workbook # pylint: disable=import-error
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
 from .utilities import status_msg
 
 
-@dataclass_json
 @dataclass
-class HourlyRate:
+class HourlyRate(DataClassJsonMixin):
     """Hourly rate by department"""
-    dept: str
     rate: float
 
+@dataclass(order=True)
+class HourlyRates(DataClassJsonMixin):
+    """Hourly Rates"""
+    hourly_rates: dict[str, HourlyRate]
 
-def load_hourly_rates(xlsx_file: Path) -> dict[str, HourlyRate]:
-    """Read consuables sheet"""
-    status_msg('Loading Consumables', 1)
+def load_hourly_rates(xlsx_file: Path) -> HourlyRates:
+    """Read hourly rates sheet"""
+    status_msg('Loading Hourly Rates', 1)
     status_msg(f'  {xlsx_file.name}', 2)
     xlsx: Workbook = load_workbook(xlsx_file.as_posix(), data_only=True)
     sheet: Worksheet = xlsx.active
-    hourly_rates: dict[str, HourlyRate] = {}
-    for row in sheet.iter_rows(min_row=2, max_col=2):
+    hourly_rate: HourlyRate
+    hourly_rates: HourlyRates = HourlyRates({})
+    for row in sheet.iter_rows(min_row=1, max_col=2):
         if not isinstance(row[0].value, str):
             continue
-        hourly_rate: HourlyRate = HourlyRate(
-            row[0].value,
-            float(row[1].value))
+        value: float = float(row[1].value)
+        name: str = row[0].value
+        hourly_rate = HourlyRate(float(value))
         status_msg(f"    {hourly_rate}", 3)
-        hourly_rates[row[0].value] = hourly_rate
+        hourly_rates.hourly_rates[name] = hourly_rate
     xlsx.close()
     return hourly_rates
 
