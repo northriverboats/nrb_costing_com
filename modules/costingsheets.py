6ec548f312e78eb7f6ac0ff8f377552fab232e54
Fredrick W. Warren <fred.warren@gmail.com>
2021-12-05 11:59:46 -0800

    refactored file
    - moved all merge and filter code to costing_merge
    - xlsx read as Bom (boat and cabin) and writen as MergedBom

--- modules/costingsheets.py
+++ modules/costingsheets.py
@@ -4,21 +4,18 @@
 """
 Generate Costing Sheets
 """
-from copy import deepcopy
 from datetime import date
 from pathlib import Path
 from xlsxwriter import Workbook # type: ignore
-# from xlsxwriter.utility import xl_rowcol_to_cell # type: ignore
-from .boms import Bom, BomPart, BomSection
+from .boms import Bom, Boms, MergedBom
 from .costing_data import FileNameInfo, SectionInfo, Xlsx, COLUMNS, STYLES
 from .costing_headers import generate_header
+from .costing_merge import get_bom
 from .costing_sections import generate_sections
 from .costing_totals import generate_totals
 from .models import Model
 from .settings import Settings
-from .utilities import (logger, normalize_size, status_msg, SHEETS_FOLDER,
-                        SUBJECT)
-from pprint import pprint
+from .utilities import normalize_size, status_msg, SHEETS_FOLDER, SUBJECT
 
 # UTILITY FUNCTIONS ===========================================================
 
@@ -55,128 +52,6 @@ def build_name(size: str, model: Model, folder: str) -> FileNameInfo:
     return name
 
 
-# BOM MANIPULATION FUNCTIONS ==================================================
-def ordered_parts(section: BomSection) -> None:
-    """set correct sort order for parts for each section. Outftting is sorted
-    by vender then part number. All other sections are ordered by part number
-
-    Arguments:
-        section: BomSection -- section name and parts
-
-    Returns:
-        list[str] -- Correctly ordered list of keys
-    """
-    if section.name != 'OUTFITTING':
-        section.parts = dict(sorted(section.parts.items(), key=lambda k: (k)))
-        return
-    section.parts = dict(sorted(section.parts.items(),
-                           key=lambda k: (k[1].vendor, k[1].part)))
-
-def bom_merge_section(target_parts: dict[str, BomPart],
-                      source_parts: dict[str, BomPart]) -> None:
-    """Merege two sections
-
-    Arguments:
-        target_parts: dict[str, BomPart] -- longer dict of BomParts to append
-                                            to or merge part with
-        source_parts: dict[str, BomPart] -- shorter dict of BomParts to add or
-                                            merge parts from
-
-    Returns:
-        None -- target_parts modified in place
-    """
-    for key in source_parts:
-        try:
-            target_part: BomPart = target_parts[key]
-            target_part.qty = ((target_part.qty  or 0) +
-                               (source_parts[key].qty or 0))
-        except KeyError:
-            target_parts[key] = deepcopy(source_parts[key])
-
-def hours_merge(sizes: dict[str, dict[str, float]],
-               hours: dict[str, float]) -> None:
-    """Merge the hours from the cabin BOM into the hours for each size of
-    boat in the bom. This has to be done now. When the merge is complete
-    the cabin infomration will not have the hours. We don't know at this
-    point what size the bom will be filted down to, so all sizes must be set.
-    hours will come from size 0
-    """
-    for size in sizes:
-        for labor in sizes[size]:
-            sizes[size][labor] += hours[labor]
-
-def bom_merge(target_bom: Bom, source_bom: Bom) -> Bom:
-    """Merge two BOMs creating a new BOM in the process. Does not modify either
-    original Bom.
-
-    Arguments:
-        target_bom: Bom -- sheet1 bom we want to merge parts into
-        source_bom: Bom -- sheet2 bom with parts to be added/merged
-
-    Returns:
-        Bom -- new merged list of both boms combined
-    """
-    bom: Bom = deepcopy(target_bom)
-    if source_bom and source_bom.sizes.get("0") == "0":
-        hours: dict[str, float] = source_bom.sizes["0"]
-        hours_merge(bom.sizes, hours)
-    for target, source in zip(bom.sections, source_bom.sections):
-        bom_merge_section(target.parts, source.parts)
-    return bom
-
-def get_bom(boms: dict[str, Bom], model: Model) -> Bom:
-    """Merges sheets if necessary and returns a BOM.
-    Assumes if sheet is not None that there will be a match
-
-    Arguments:
-        bom: list[Bom] --
-        model: Model -- sheet1 can not be None and must be found
-                        sheet2 can be None but *must* be found if not None
-
-    Returns:
-        Bom -- Returns new Bom of combined Bom(s)
-    """
-    bom1: Bom = (boms[model.sheet1]
-                 if model.sheet1 in boms
-                 else  Bom('', "", 0.0, 0.0, {}, []))
-    bom2: Bom = (boms[model.sheet2]
-                 if model.sheet2 in boms
-                 else  Bom('', "", 0.0, 0.0, {}, []))
-    if bom1.name == "":
-        logger.debug("bom1 not found error %s", model.sheet1)
-    if bom2.name == "":
-        # logger.debug("bom2 not found error %s %s  %s",
-        #             model.sheet1, model.sheet2, model.folder)
-        return bom1
-    return bom_merge(bom1, bom2)
-
-def filter_bom(original_bom: Bom, size: str) -> Bom:
-    """fitler out parts based on size if necessary
-    also correcting costs as necessary
-
-    Arguments:
-        bom: Bom -- Bom with parts sections
-        size: float -- size of boat to filter for
-
-    Returns:
-        Bom -- Deepcopy Bom with correct parts
-    """
-    bom: Bom = deepcopy(original_bom)
-    for section in bom.sections:
-        section.parts = {
-            k:v
-            for k, v in section.parts.items()
-            if ((v.smallest or 0) == 0 or float(size) >= (v.smallest or 0)) and
-            ((v.biggest or 0) == 0 or float(size) <= (v.biggest or 0))
-        }
-        for v in section.parts.values():
-            if v.percent:
-                v.qty = (
-                    float(size) / (v.percent or 8) * (v.qty or 0))
-        ordered_parts(section)
-    return bom
-
-
 # WRITING SHEET FUNCTIONS =====================================================
 def properties(xlsx):
     """set sheet properties"""
@@ -189,7 +64,7 @@ def properties(xlsx):
         'comments': 'Created with Python and XlsxWriter',
     }
 
-def generate_sheet(filtered_bom: Bom,
+def generate_sheet(merged_bom: MergedBom,
                    file_name_info: FileNameInfo,
                    settings: Settings,
                    size: str) -> None:
@@ -212,7 +87,7 @@ def generate_sheet(filtered_bom: Bom,
     section_info: dict[str, SectionInfo] =  {}
     with Workbook(file_name_info['file_name'],
                   {'remove_timezone': True}) as workbook:
-        xlsx: Xlsx = Xlsx(workbook, filtered_bom, size, settings)
+        xlsx: Xlsx = Xlsx(workbook, merged_bom, size, settings)
 
         xlsx.file_name_info = file_name_info
         xlsx.workbook.set_properties(properties(xlsx))
@@ -229,8 +104,8 @@ def generate_sheet(filtered_bom: Bom,
 
 
 # MODEL/SIZE IETERATION FUNCTIONS =============================================
-def generate_sheets_for_model(model: Model,
-                              bom: Bom,
+def generate_sheets_for_model(boms: Boms,
+                              model: Model,
                               settings: Settings) -> None:
     """"cycle through each size to create sheets
     * build the filname and size as as a text name
@@ -239,20 +114,20 @@ def generate_sheets_for_model(model: Model,
     * computing section sizes is done in genereate_sheet
 
     Arguments:
+        boms --  all boats/cabin boms
         model -- Model of boat to process
-        bom -- Base Bom for model
         settings -- consumables, labor rates, mark ups
 
     Returns:
         None
     """
     status_msg(f"  {model.folder}", 1)
-    for size in bom.sizes:
+    for size in boms[model.sheet1].sizes:
         file_name_info: FileNameInfo
         file_name_info = build_name(size, model, model.folder)
         status_msg(f"    {file_name_info['file_name']}", 2)
-        filtered_bom = filter_bom(bom, size)
-        generate_sheet(filtered_bom, file_name_info, settings, str(size))
+        merged_bom: MergedBom = get_bom(boms, model, size)
+        generate_sheet(merged_bom, file_name_info, settings, str(size))
 
 
 def generate_sheets_for_all_models(models: dict[str, Model],
@@ -270,8 +145,7 @@ def generate_sheets_for_all_models(models: dict[str, Model],
     """
     status_msg("Saving Sheets", 1)
     for model in models:  # fww
-        bom: Bom = get_bom(boms, models[model])
-        generate_sheets_for_model(models[model], bom, settings)
+        generate_sheets_for_model(boms, models[model], settings)
 
 if __name__ == "__main__":
     pass
